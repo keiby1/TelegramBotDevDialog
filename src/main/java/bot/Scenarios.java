c69bb236a2fc900df6a6c2b6bb05411fff7fa36c
Alex <lushinaleksei@bk.ru>
2021-08-31 22:24:38 +0400

    Дорабатываю сценарии

--- src/main/java/bot/Scenarios.java
+++ src/main/java/bot/Scenarios.java
@@ -25,14 +25,17 @@ public class Scenarios {
         listCommands.add(new Command("/project", ""));
         listCommands.add(new Command("/About", ""));
 
-        listCommands.add(new Command("/API", ""));
-        listCommands.add(new Command("/Protocols", ""));
+        listCommands.add(new Command("/API", "В системе реализовано 1 апи /transfer"));
+        listCommands.add(new Command("/Protocols", "Протокол http"));
         listCommands.add(new Command("/Example", ""));
-        listCommands.add(new Command("/Integrations", ""));
+        listCommands.add(new Command("/Integrations", "У системы есть интеграция с системой проаеряющей валидность операции. Система ожидаетзапрос по пути /validation"));
+        listCommands.add(new Command("/IntegrationsExample", "Пример запроса: body{}"));
+        listCommands.add(new Command("/IntegrationsExample", "Пример ответа: body{}"));
+        listCommands.add(new Command("/IntegrationsExample", "В среднем эта система отвечает за 2 секунды"));
 
-        listCommands.add(new Command("/Requirements", ""));
-        listCommands.add(new Command("/SLA", ""));
-        listCommands.add(new Command("/Performance", ""));
+        listCommands.add(new Command("/Requirements", "Требования по Временам отклика /SLA и по запасу производителности /Performance"));
+        listCommands.add(new Command("/SLA", "Операции должны выполняться за 3сек"));
+        listCommands.add(new Command("/Performance", "Система должна выдерживать нагрузку в 100тпс"));
 
         listCommands.add(new Command("/Somethingelse", ""));
     }
